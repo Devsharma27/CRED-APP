7226990b59b1ca558d8520347770e0378fde1eb6
Devsharma27 <94638730+Devsharma27@users.noreply.github.com>
2021-12-21 01:43:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+import logging
 
 app = Flask(__name__)
+
+logging.basicConfig(filename='demo.log', level=logging.DEBUG)
+
 @app.route('/')
 def hello():
+    app.logger.error('Processing default request again and again')
+    app.logging.info("A Info Logging Message")
     return 'hello world Dev!'
-
 if __name__== "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=80)
