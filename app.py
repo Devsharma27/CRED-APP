b63679f7da070ec8797746c79896fc0d972beb5e
Devsharma27 <94638730+Devsharma27@users.noreply.github.com>
2021-12-21 13:29:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 import logging
 
 app = Flask(__name__)
-logging.basicConfig(level=logging.INFO)
 @app.route('/')
 def hello():
     app.logger.error('Processing default request again and again')
